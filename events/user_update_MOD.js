168d8cf50001414535636077c27c45ffddc762da
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-06-18 16:38:49 +0200

    Create user_update_MOD.js
    Member Update from DBM triggers on `guildMemberUpdate`, this triggers on `userUpdate`.

---
+++ events/user_update_MOD.js
@@ -0,0 +1,22 @@
+module.exports = {
+name: "User Update",
+isEvent: true,
+fields: ["User Before Update", "User After Update"],
+mod: function(DBM) {
+	DBM.RigidKeK = DBM.RigidKeK || {};
+	DBM.RigidKeK.callUserUpdate = function(pre, post) {
+		const { Bot, Actions } = DBM;
+		const events = Bot.$evts["User Update"];
+		if(!events) return;
+		for(let i = 0; i < events.length; i++) {
+			const event = events[i];
+			const temp = {};
+			if(event.temp) temp[event.temp] = pre;
+			if(event.temp2) temp[event.temp2] = post;
+			const server = null;
+			Actions.invokeEvent(event, server, temp);
+		}
+	};
+	DBM.Bot.bot.on('userUpdate', DBM.RigidKeK.callUserUpdate);
+}
+};
