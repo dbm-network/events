65f9f63bba35fd933acdca5178fc714778645bc7
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-04 22:01:10 +0200

    single namespace

--- events/member_leave_voice_channel_EVT.js
+++ events/member_leave_voice_channel_EVT.js
@@ -1,45 +1,17 @@
 module.exports = {
-	/**
-   * The author of the event.
-   * @type {string}
-  */
 	author: "Almeida",
 
-	/**
-   * The name of the event type on the editor.
-   * @type {string}
-  */
 	name: "Member Leave Voice Channel",
 
-	/**
-   * Whether the object is of an event or not.
-   * @type {boolean}
-  */
 	isEvent: true,
 
-	/**
-   * The fields of the event (Variables); there can only be either: 0, 1 or 2.
-   * @type {Array<string>}
-  */
 	fields: ["Temp Variable Name (stores member that entered the channel):", "Temp Variable Name (stores channel that the member left):"],
 
-	/**
-   * The function that is ran when the software/bot starts.
-   * @param {Object<*>} DBM The DBM object.
-   * @return {void}
-   */
 	mod(DBM) {
-		DBM.MemberLeaveVoiceChannel = DBM.MemberLeaveVoiceChannel || {};
-
+		DBM.events = DBM.events || {};
 		const { Actions, Bot } = DBM;
 
-		/**
-     * Runs through all the bots events and runs the one that apply.
-     * @param {User} oldUser The member before the voice state update.
-     * @param {User} newUser The member after the voice state update.
-     * @return {void}
-     */
-		DBM.MemberLeaveVoiceChannel.callAllEvents = function(oldVoiceState, newVoiceState) {
+		DBM.events.MemberLeaveVoiceChannel = function(oldVoiceState, newVoiceState) {
 			const events = Bot.$evts["Member Leave Voice Channel"];
 			if (!events) return;
 
@@ -57,12 +29,9 @@ module.exports = {
 			}
 		};
 
-		/*
-     * This is required so we have access to the Discord Client.
-     */
 		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("voiceStateUpdate", DBM.MemberLeaveVoiceChannel.callAllEvents);
+		Bot.onReady = function(...params) {
+			Bot.bot.on("voiceStateUpdate", DBM.events.MemberLeaveVoiceChannel);
 			onReady.apply(this, ...params);
 		};
 	},
