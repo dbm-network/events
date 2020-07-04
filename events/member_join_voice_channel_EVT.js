b5bd443a7621a494c7fe2ebc96c82075b78e7359
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-04 21:58:58 +0200

    single namespace

--- events/member_join_voice_channel_EVT.js
+++ events/member_join_voice_channel_EVT.js
@@ -1,45 +1,12 @@
 module.exports = {
-	/**
-   * The author of the event.
-   * @type {string}
-  */
 	author: "Almeida",
-
-	/**
-   * The name of the event type on the editor.
-   * @type {string}
-  */
 	name: "Member Join Voice Channel",
-
-	/**
-   * Whether the object is of an event or not.
-   * @type {boolean}
-  */
 	isEvent: true,
-
-	/**
-   * The fields of the event (Variables); there can only be either: 0, 1 or 2.
-   * @type {Array<string>}
-  */
 	fields: ["Temp Variable Name (stores member that entered the channel):", "Temp Variable Name (stores channel that the member joined):"],
-
-	/**
-   * The function that is ran when the software/bot starts.
-   * @param {Object<*>} DBM The DBM object.
-   * @return {void}
-   */
 	mod(DBM) {
-		DBM.MemberJoinVoiceChannel = DBM.MemberJoinVoiceChannel || {};
-
+		DBM.events = DBM.events || {};
 		const { Actions, Bot } = DBM;
-
-		/**
-     * Runs through all the bots events and runs the one that apply.
-     * @param {User} oldUser The member before the voice state update.
-     * @param {User} newUser The member after the voice state update.
-     * @return {void}
-     */
-		DBM.MemberJoinVoiceChannel.callAllEvents = function(oldVoiceState, newVoiceState) {
+		DBM.events.MemberJoinVoiceChannel = function(oldVoiceState, newVoiceState) {
 			const events = Bot.$evts["Member Join Voice Channel"];
 			if (!events) return;
 
@@ -57,12 +24,9 @@ module.exports = {
 			}
 		};
 
-		/*
-     * This is required so we have access to the Discord Client.
-     */
 		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("voiceStateUpdate", DBM.MemberJoinVoiceChannel.callAllEvents);
+		Bot.onReady = function(...params) {
+			Bot.bot.on("voiceStateUpdate", DBM.events.MemberJoinVoiceChannel);
 			onReady.apply(this, ...params);
 		};
 	},
