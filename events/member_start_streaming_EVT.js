109b94bdc8e8be8e755dec5a25df9ff9f7278701
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:29:17 +0200

    eslint issues

--- events/member_start_streaming_EVT.js
+++ events/member_start_streaming_EVT.js
@@ -1,35 +1,32 @@
 module.exports = {
 
-	name: "Member Start Streaming",
+  name: 'Member Start Streaming',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (Store voice channel):", "Temp Variable Name (Store streaming member object):"],
+  fields: ['Temp Variable Name (Store voice channel):', 'Temp Variable Name (Store streaming member object):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.onStream = function(oldVoiceState, newVoiceState) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Member Start Streaming"];
-			if (!events) return;
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    DBM.events.onStream = function (oldVoiceState, newVoiceState) {
+      const { Bot, Actions } = DBM
 
-			const oldChannel = oldVoiceState.channel;
-			const newChannel = newVoiceState.channel;
-			if ((!oldChannel || !newChannel) || (oldVoiceState.streaming && !newVoiceState.streaming)) return;
-			const server = (oldChannel || newChannel).guild;
+      const oldChannel = oldVoiceState.channel
+      const newChannel = newVoiceState.channel
+      if ((!oldChannel || !newChannel) || (oldVoiceState.streaming && !newVoiceState.streaming)) return
+      const server = (oldChannel || newChannel).guild
+      Bot.$evts['Member Start Streaming'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = newChannel
+        if (event.temp2) temp[event.temp2] = newVoiceState.member
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
 
-			for (const event of events) {
-				const temp = {};
-				if (event.temp) temp[event.temp] = newChannel;
-				if (event.temp2) temp[event.temp2] = newVoiceState.member;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("voiceStateUpdate", DBM.events.onStream);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+    const onReady = DBM.Bot.onReady
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('voiceStateUpdate', DBM.events.onStream)
+      onReady.apply(this, ...params)
+    }
+  }
+}
