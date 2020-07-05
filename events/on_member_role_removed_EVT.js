a0cb573d07786f4b3c20a2d98a65d437a7dd7f90
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:48:30 +0200

    eslint issues

--- events/on_member_role_removed_EVT.js
+++ events/on_member_role_removed_EVT.js
@@ -1,38 +1,34 @@
 module.exports = {
-	name: "Member Role Removed MOD",
+  name: 'Member Role Removed MOD',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (Stores role object):", "Temp Variable Name (Stores member object):"],
+  fields: ['Temp Variable Name (Stores role object):', 'Temp Variable Name (Stores member object):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.roleRemoved = async function(oldMember, newMember) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Member Role Removed MOD"];
-			if(!events) return;
-			if (newMember.roles.size > oldMember.roles.size) return;
-			const server = newMember.guild;
-			for (const event of events) {
-				const temp = {};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    const { Bot, Actions } = DBM
+    DBM.events.roleRemoved = async function (oldMember, newMember) {
+      const oldRoles = oldMember.roles
+      const newRoles = newMember.roles
 
-				const oldRoles = oldMember.roles;
-				const newRoles = newMember.roles;
+      const difference = oldRoles.filter((role) => !newRoles.has(role.id)).first()
+      if (newMember.roles.size > oldMember.roles.size) return
+      const server = newMember.guild
+      Bot.$evts['Member Role Removed MOD'].forEach((event) => {
+        const temp = {}
 
-				let difference = oldRoles.filter((role) => !newRoles.has(role.id)).first();
-				if (event.temp) temp[event.temp] = difference;
-				if (event.temp2) temp[event.temp2] = newMember;
+        if (event.temp) temp[event.temp] = difference
+        if (event.temp2) temp[event.temp2] = newMember
 
-				Actions.invokeEvent(event, server, temp);
-			}
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
 
-
-		};
-
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("guildMemberUpdate", DBM.events.roleRemoved);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+    const onReady = DBM.Bot.onReady
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('guildMemberUpdate', DBM.events.roleRemoved)
+      onReady.apply(this, ...params)
+    }
+  }
+}
