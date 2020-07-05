a70c6abdc020a0cea6940c6a30391e79f0782763
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:50:19 +0200

    eslint issues

--- events/user_update_EVT.js
+++ events/user_update_EVT.js
@@ -1,26 +1,23 @@
 module.exports = {
-	name: "User Update",
-	isEvent: true,
-	fields: ["User Before Update (Temp Variable Name):", "User After Update (Temp Variable Name):"],
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		const { Bot, Actions } = DBM;
-		DBM.events.callUserUpdate = function(pre, post) {
-			const events = Bot.$evts["User Update"];
-			if (!events) return;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = pre;
-				if(event.temp2) temp[event.temp2] = post;
-				const server = null;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-		const onReady = DBM.Bot.onReady;
-		Bot.onReady = function(...params) {
-			Bot.bot.on("userUpdate", DBM.events.callUserUpdate);
-			onReady.apply(this, ...params);
-		};
+  name: 'User Update',
+  isEvent: true,
+  fields: ['User Before Update (Temp Variable Name):', 'User After Update (Temp Variable Name):'],
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    const { Bot, Actions } = DBM
+    DBM.events.callUserUpdate = function (pre, post) {
+      Bot.$evts['User Update'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = pre
+        if (event.temp2) temp[event.temp2] = post
 
-	}
-};
+        Actions.invokeEvent(event, null, temp)
+      })
+    }
+    const onReady = Bot.onReady
+    Bot.onReady = function (...params) {
+      Bot.bot.on('userUpdate', DBM.events.callUserUpdate)
+      onReady.apply(this, ...params)
+    }
+  }
+}
