ac3dfc6a6e1686adbc144973d150903c610bbca4
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:44:09 +0200

    eslint issues

--- events/on_member_nickname_changed_EVT.js
+++ events/on_member_nickname_changed_EVT.js
@@ -1,30 +1,28 @@
 module.exports = {
-	name: "Member Nickname Changed MOD",
+  name: 'Member Nickname Changed MOD',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (Stores new nickname):", "Temp Variable Name (Stores member object):"],
+  fields: ['Temp Variable Name (Stores new nickname):', 'Temp Variable Name (Stores member object):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		const { Bot, Actions } = DBM;
-		DBM.events.nicknameChanged = async function(oldMember, newMember) {
-			const events = Bot.$evts["Member Nickname Changed MOD"];
-			if(!events) return;
-			if (newMember.nickname === oldMember.nickname) return;
-			const server = newMember.guild;
-			for (const event of events) {
-				const temp = {};
-				if (event.temp) temp[event.temp] = newMember.nickname;
-				if (event.temp2) temp[event.temp2] = newMember;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    const { Bot, Actions } = DBM
+    DBM.events.nicknameChanged = async function (oldMember, newMember) {
+      if (newMember.nickname === oldMember.nickname) return
+      const server = newMember.guild
+      Bot.$evts['Member Nickname Changed MOD'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = newMember.nickname
+        if (event.temp2) temp[event.temp2] = newMember
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
 
-		const onReady = DBM.Bot.onReady;
-		Bot.onReady = function(...params) {
-			Bot.bot.on("guildMemberUpdate", DBM.events.nicknameChanged);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+    const onReady = Bot.onReady
+    Bot.onReady = function (...params) {
+      Bot.bot.on('guildMemberUpdate', DBM.events.nicknameChanged)
+      onReady.apply(this, ...params)
+    }
+  }
+}
