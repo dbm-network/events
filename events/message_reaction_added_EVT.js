fb44159bba67d964d1f110e8904aac561972a5a3
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:37:00 +0200

    eslint issues

--- events/message_reaction_added_EVT.js
+++ events/message_reaction_added_EVT.js
@@ -1,30 +1,37 @@
 module.exports = {
 
-	name: "Message Reaction Added MOD",
+  name: 'Message Reaction Added MOD',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Reaction (Temp Variable Name):", "Member who Reacted (Temp Variable Name):"],
+  fields: ['Reaction (Temp Variable Name):', 'Member who Reacted (Temp Variable Name):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.reactionAdded = function(reaction, member) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Message Reaction Added MOD"];
-			if(!events) return;
-			const server = reaction.message.guild;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = reaction;
-				if(event.temp2) temp[event.temp2] = member;
-				if(event.temp2) temp[event.temp2] = server.members.get(member.id);
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("messageReactionAdd", DBM.events.reactionAdded);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+
+    DBM.events.reactionAdded = function (reaction, member) {
+      const { Bot, Actions } = DBM
+
+      const server = reaction.message.guild
+
+      Bot.$evts['Message Reaction Added MOD'].forEach((event) => {
+        const temp = {}
+
+        if (event.temp) temp[event.temp] = reaction
+
+        if (event.temp2) temp[event.temp2] = server.members.cache.get(member.id)
+
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
+
+    const onReady = DBM.Bot.onReady
+
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('messageReactionAdd', DBM.events.reactionAdded)
+
+      onReady.apply(this, ...params)
+    }
+  }
+
+}
