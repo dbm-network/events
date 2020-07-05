de1df7eba156a28c45f17fc662f25e19454006fc
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:39:12 +0200

    eslint issues

--- events/message_reaction_removed_EVT.js
+++ events/message_reaction_removed_EVT.js
@@ -1,29 +1,27 @@
 module.exports = {
 
-	name: "Message Reaction Removed MOD",
+  name: 'Message Reaction Removed MOD',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Reaction (Temp Variable Name):", "Member who Reacted (Temp Variable Name):"],
+  fields: ['Reaction (Temp Variable Name):', 'Member who Reacted (Temp Variable Name):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.reactionRemoved = function(reaction, member) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Message Reaction Removed MOD"];
-			if(!events) return;
-			const server = reaction.message.guild;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = reaction;
-				if(event.temp2) temp[event.temp2] = server.members.get(member.id);
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("messageReactionRemove", DBM.events.reactionRemoved);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    const { Bot, Actions } = DBM
+    DBM.events.reactionRemoved = function (reaction, member) {
+      const server = reaction.message.guild
+      Bot.$evts['Message Reaction Removed MOD'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = reaction
+        if (event.temp2) temp[event.temp2] = server.members.get(member.id)
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
+    const onReady = DBM.Bot.onReady
+    Bot.onReady = function (...params) {
+      Bot.bot.on('messageReactionRemove', DBM.events.reactionRemoved)
+      onReady.apply(this, ...params)
+    }
+  }
+}
