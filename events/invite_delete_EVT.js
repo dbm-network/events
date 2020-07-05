0f5623ce76b4f30e19d57762221cceb3962e4ef8
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:09:47 +0200

    eslint issues

--- events/invite_delete_EVT.js
+++ events/invite_delete_EVT.js
@@ -1,28 +1,26 @@
 module.exports = {
-	name: "Invite Delete",
+  name: 'Invite Delete',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (stores invite code that was deleted):"],
+  fields: ['Temp Variable Name (stores invite code that was deleted):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.inviteDelete = function(invite) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Invite Delete"];
-			if(!events) return;
-			const server = invite.guild;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = invite.code;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    DBM.events.inviteDelete = function (invite) {
+      const { Bot, Actions } = DBM
+      const server = invite.guild
+      Bot.$evts['Invite Delete'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = invite.code
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
 
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("inviteDelete", DBM.events.inviteDelete);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+    const onReady = DBM.Bot.onReady
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('inviteDelete', DBM.events.inviteDelete)
+      onReady.apply(this, ...params)
+    }
+  }
+}
