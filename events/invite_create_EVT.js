c0d5cdc454f069b1d98d40304ddc39b8c105cd49
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:07:15 +0200

    eslint issues

--- events/invite_create_EVT.js
+++ events/invite_create_EVT.js
@@ -1,28 +1,26 @@
 module.exports = {
-	name: "Invite Create",
+  name: 'Invite Create',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (stores invite code):", "Temp Variable Name (stores creator of invite):"],
+  fields: ['Temp Variable Name (stores invite code):', 'Temp Variable Name (stores creator of invite):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.inviteCreate = function(invite) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Invite Create"];
-			if(!events) return;
-			const server = invite.guild;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = invite.code;
-				if(event.temp2) temp[event.temp2] = invite.inviter;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("inviteCreate", DBM.events.inviteCreate);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    DBM.events.inviteCreate = function (invite) {
+      const { Bot, Actions } = DBM
+      const server = invite.guild
+      Bot.$evts['Invite Create'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = invite.code
+        if (event.temp2) temp[event.temp2] = invite.inviter
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
+    const onReady = DBM.Bot.onReady
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('inviteCreate', DBM.events.inviteCreate)
+      onReady.apply(this, ...params)
+    }
+  }
+}
