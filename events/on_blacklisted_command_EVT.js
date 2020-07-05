4f6b7a39169965b6105fcdb35a61ebcb781170aa
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:41:41 +0200

    Update on_blacklisted_command_EVT.js

--- events/on_blacklisted_command_EVT.js
+++ events/on_blacklisted_command_EVT.js
@@ -1,25 +1,33 @@
 module.exports = {
-	name: "On Blacklisted Command",
-	isEvent: true,
-	fields: ["User Who Used Command", "Command Message"],
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.blacklistedUserUse = function(user, message) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["On Blacklisted Command"];
-			if (!events) return;
-			for (const event of events) {
-				const temp = {};
-				if(event.temp) temp[event.temp] = user;
-				if(event.temp2) temp[event.temp2] = message;
-				const server = user.guild || null;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("blacklistUserUse", DBM.events.blacklistedUserUse);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+
+  name: 'On Blacklisted Command',
+
+  isEvent: true,
+
+  fields: ['User Who Used Command', 'Command Message'],
+
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    const { Bot, Actions } = DBM
+    DBM.events.blacklistedUserUse = function (user, message) {
+      const server = user.guild || null
+      Bot.$evts['On Blacklisted Command'].forEach((event) => {
+        const temp = {}
+
+        if (event.temp) temp[event.temp] = user
+        if (event.temp2) temp[event.temp2] = message
+
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
+
+    const onReady = Bot.onReady
+
+    Bot.onReady = function (...params) {
+      Bot.bot.on('blacklistUserUse', DBM.events.blacklistedUserUse)
+
+      onReady.apply(this, ...params)
+    }
+  }
+
+}
