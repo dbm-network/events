481f022f6180cbafaa43956a3d1ec3f551ca4f3f
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:18:44 +0200

    Update member_join_voice_channel_EVT.js

--- events/member_join_voice_channel_EVT.js
+++ events/member_join_voice_channel_EVT.js
@@ -1,33 +1,36 @@
 module.exports = {
-	author: "Almeida",
-	name: "Member Join Voice Channel",
-	isEvent: true,
-	fields: ["Temp Variable Name (stores member that entered the channel):", "Temp Variable Name (stores channel that the member joined):"],
-	mod(DBM) {
-		DBM.events = DBM.events || {};
-		const { Actions, Bot } = DBM;
-		DBM.events.MemberJoinVoiceChannel = function(oldVoiceState, newVoiceState) {
-			const events = Bot.$evts["Member Join Voice Channel"];
-			if (!events) return;
 
-			for (const event of events) {
-				const temp = {};
+  author: 'Almeida',
 
-				const oldChannel = oldVoiceState.channel;
-				const newChannel = newVoiceState.channel;
-				const server = (oldChannel || newChannel).guild;
+  name: 'Member Join Voice Channel',
 
-				if (event.temp) temp[event.temp] = newVoiceState.member;
-				if (event.temp2) temp[event.temp2] = newChannel;
+  isEvent: true,
 
-				if (!oldChannel && newChannel) Actions.invokeEvent(event, server, temp);
-			}
-		};
+  fields: ['Temp Variable Name (stores member that entered the channel):', 'Temp Variable Name (stores channel that the member joined):'],
 
-		const onReady = DBM.Bot.onReady;
-		Bot.onReady = function(...params) {
-			Bot.bot.on("voiceStateUpdate", DBM.events.MemberJoinVoiceChannel);
-			onReady.apply(this, ...params);
-		};
-	},
-};
+  mod (DBM) {
+    DBM.events = DBM.events || {}
+
+    const { Actions, Bot } = DBM
+
+    DBM.events.MemberJoinVoiceChannel = function (oldVoiceState, newVoiceState) {
+      const oldChannel = oldVoiceState.channel
+      const newChannel = newVoiceState.channel
+      const server = (oldChannel || newChannel).guild
+      Bot.$evts['Member Join Voice Channel'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = newVoiceState.member
+        if (event.temp2) temp[event.temp2] = newChannel
+        if (!oldChannel && newChannel) Actions.invokeEvent(event, server, temp)
+      })
+    }
+
+    const onReady = DBM.Bot.onReady
+    Bot.onReady = function (...params) {
+      Bot.bot.on('voiceStateUpdate', DBM.events.MemberJoinVoiceChannel)
+
+      onReady.apply(this, ...params)
+    }
+  }
+
+}
