7d54003923886dbe1eb6c41aff8e351cb329f16b
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-05 15:04:44 +0200

    eslint issues

--- events/delete_bulk_messages_EVT.js
+++ events/delete_bulk_messages_EVT.js
@@ -1,31 +1,27 @@
 module.exports = {
 
-	name: "Delete Bulk Messages",
+  name: 'Delete Bulk Messages',
 
-	isEvent: true,
+  isEvent: true,
 
-	fields: ["Temp Variable Name (stores list of messages):", "Temp Variable Name (stores amount of messages):"],
+  fields: ['Temp Variable Name (stores list of messages):', 'Temp Variable Name (stores amount of messages):'],
 
-	mod: function(DBM) {
-		DBM.events = DBM.events || {};
-		DBM.events.messageDeleteBulk = function(messagesList) {
-			const { Bot, Actions } = DBM;
-			const events = Bot.$evts["Delete Bulk Messages"];
-			if(!events) return;
-			const server = messagesList.first().guild;
-			for(let i = 0; i < events.length; i++) {
-				const temp = {};
-				const event = events[i];
-				if(event.temp) temp[event.temp] = messagesList.array();
-				if(event.temp2) temp[event.temp2] = messagesList.size;
-				Actions.invokeEvent(event, server, temp);
-			}
-		};
-
-		const onReady = DBM.Bot.onReady;
-		DBM.Bot.onReady = function(...params) {
-			DBM.Bot.bot.on("messageDeleteBulk", DBM.events.messageDeleteBulk);
-			onReady.apply(this, ...params);
-		};
-	}
-};
+  mod: function (DBM) {
+    DBM.events = DBM.events || {}
+    DBM.events.messageDeleteBulk = function (messagesList) {
+      const { Bot, Actions } = DBM
+      const server = messagesList.first().guild
+      Bot.$evts['Delete Bulk Messages'].forEach((event) => {
+        const temp = {}
+        if (event.temp) temp[event.temp] = messagesList.array()
+        if (event.temp2) temp[event.temp2] = messagesList.size
+        Actions.invokeEvent(event, server, temp)
+      })
+    }
+    const onReady = DBM.Bot.onReady
+    DBM.Bot.onReady = function (...params) {
+      DBM.Bot.bot.on('messageDeleteBulk', DBM.events.messageDeleteBulk)
+      onReady.apply(this, ...params)
+    }
+  }
+}
